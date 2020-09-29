b483fa389d9227c4e203d2b951d739ff7c3d2d61
Zaafira Hasan <zfh4@njit.edu>
2020-09-29 16:46:29 -0400

    creating databases

---
+++ dbtest/lib/config.php
@@ -0,0 +1,13 @@
+<?php
+//this file you'll have to create directly on the VM or use FTP to move it
+//this should not and will not be on github, -points if it's seen on github.
+
+//for Heroku use the below segment and delete the duplicate below
+
+//for everyone else fill in these variable strings with the connection
+//details from your respective servers and delete the Heroku block above
+$dbhost = "zfh4@sql1.njit.edu";//this should be fine for all but NJIT, which uses "sql1.njit.edu" or "sql2.njit.edu" or "sql3.njit.edu"
+$dbuser = "zfh4";//your ucid since we follow this pattern
+$dbpass = "Rmalam123456!";//database password generated or chosen per respective steps
+$dbdatabase = "zfh4";//your ucid since we follow this pattern
+?>
